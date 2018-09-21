019282a9c59fd15232d0db8d433c2c2a56b6ecef
John Liu <bmzyDream@gmail.com>
2018-09-21 16:13:08 -0700

    aaa will be merged

--- Unix/tests/codec/mof/blue/test_lex.cpp
+++ Unix/tests/codec/mof/blue/test_lex.cpp
@@ -28,20 +28,11 @@
 
 #define TEST_ASSERT(x) NitsAssert(x, MI_T(""))
 
-#if defined(sun) || defined(hpux) || defined(aix)
-
-// 
-// This failes due to difficulty supporting four byte character sets in codec/mof/parser/utility.c
-// An issue has been filed.
-//
-#define DISABLE_CHAR_TESTS 1
-#endif
-
 using namespace std;
 
 static void TestLexParser(LEX_TEST *test)
 {
-#if !DISABLE_CHAR_TESTS
+#if !DISABLE_WCHAR_TESTS
     MOF_Parser * parser = MI_MOFParser_Init((void*)test->buf, (MI_Uint32)test->size, NULL);
     MOF_State * state = (MOF_State*)parser->state;
     LEX_RESULT *r = test->expected;
